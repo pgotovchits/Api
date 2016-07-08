fc8591774bef48a8f0ebf0d164d69fbb602a82dd
Alexey Svetliakov <asvetliakov@teledini.com>
2016-07-08 08:52:25 +0200

    Index file

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// Utils
+export * from "./utils/Errors";
+
+// Realtime interfaces
+export * from "./interfaces/realtime/auth";
+export * from "./interfaces/realtime/communication";
+export * from "./interfaces/realtime/history";
+export * from "./interfaces/realtime/invite";
+export * from "./interfaces/realtime/note";
+export * from "./interfaces/realtime/response";
+export * from "./interfaces/realtime/user";
+export * from "./interfaces/realtime/visitor";
+export * from "./interfaces/realtime/website";
+
+// Events
+export * from "./events/realtimeEvents";
+
+// Actions
+
+export * from "./actions/action";
+export * from "./actions/realtimeAction";
+export * from "./actions/auth/constants";
+export * from "./actions/communication/constants";
+export * from "./actions/history/constants";
+export * from "./actions/invite/constants";
+export * from "./actions/note/constants";
+export * from "./actions/user/constants";
+export * from "./actions/website/constants";
