db002546b61258b314f647f161d59f214e944dea
Alexey Svetliakov <asvetliakov@teledini.com>
2017-09-12 18:23:31 +0200

    Add page to TrackVisitRequestPayload

--- src/visitor/1.1/tracking.ts
+++ src/visitor/1.1/tracking.ts
@@ -4,7 +4,12 @@ import { VISITOR_TRACK_VISIT } from "../constants";
 /**
  * Track visit request. Now is empty but may be expanded later
  */
-export interface TrackVisitRequestPayload { }
+export interface TrackVisitRequestPayload {
+    /**
+     * Visit page to track
+     */
+    visitPage?: string;
+}
 
 /**
  * Track visitor visit action. For now it's just increasing the counter of "Links clicked" in portal
